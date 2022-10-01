52cb55fc642eefd19f5886f3bfafe50afb74dd7d
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-10-01 13:54:01 -0400

    add support for multiple shapes with csg

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2D Ray Tracer
+
+![CSG Intersection Image](screenshots/csg.png)
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import frag from './frag.glsl';
 
 const W = 1000;
 const H = 1000;
-const N = 64;
+const N = 256;
 
 const MAX_STEP = 10;
 const MAX_DISTANCE = 2;
@@ -28,10 +28,33 @@ function handleResize() {
     renderer.setSize(W, H);
 }
 
+function union(a, b) {
+    return a.sd < b.sd ? a : b;
+}
+
+function intersect(a, b) {
+    const r = a.sd > b.sd ? b : a;
+    r.sd = a.sd > b.sd ? a.sd: b.sd;
+    return r;
+}
+
+function subtract(a, b) {
+    const r = a;
+    r.sd = (a.sd > -b.sd) ? a.sd : -b.sd;
+    return r;
+}
+
 function circleSDF(x, y, cx, cy, r) {
     const ux = x - cx;
     const uy = y - cy;
     return Math.sqrt(ux * ux  + uy * uy) - r;
+}
+
+function scene(x, y) {
+    const r1 = { sd: circleSDF(x, y, 0.4, 0.5, 0.2), emissive: 1 };
+    const r2 = { sd: circleSDF(x, y, 0.6, 0.5, 0.2), emissive: 0.8 };
+
+    return subtract(r1, r2);
 }
 
 function render() {
@@ -67,11 +90,11 @@ function sample(x, y) {
 function trace(x, y, dx, dy) {
     let t = 0;
     for (let i = 0; i < MAX_STEP && t < MAX_DISTANCE; i++) {
-        const sd = circleSDF(x + dx * t, y + dy * t, 0.5, 0.5, 0.1);
-        if (sd < Number.EPSILON) {
-            return 2;
+        const r = scene(x + dx * t, y + dy * t);
+        if (r.sd < Number.EPSILON) {
+            return r.emissive;
         }
-        t += sd;
+        t += r.sd;
     }
     return 0;
 }
