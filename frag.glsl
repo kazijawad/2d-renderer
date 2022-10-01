459063aca7e5f19c5bc295915be0737f2b432b1e
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-10-01 14:54:33 -0400

    switch sampling to gpu for speeed

--- frag.glsl
+++ frag.glsl
@@ -1,14 +1,101 @@
 #version 300 es
 
+#define TWO_PI 6.28318530718f
+#define EPSILON 1e-6f
+
+#define W 1000
+#define H 1000
+#define N 128
+
+#define MAX_STEP 10
+#define MAX_DISTANCE 2.0
+
 precision highp float;
 
 uniform vec2 resolution;
-uniform sampler2D map;
 
 out vec4 fragColor;
 
+struct Shape {
+    float sd;
+    float emissive;
+};
+
+Shape unionOP(Shape a, Shape b) {
+    if (a.sd < b.sd) {
+        return a;
+    }
+    return b;
+}
+
+Shape intersectOP(Shape a, Shape b) {
+    Shape r;
+    if (a.sd > b.sd) {
+        r = b;
+        r.sd = a.sd;
+    } else {
+        r = a;
+        r.sd = b.sd;
+    }
+    return r;
+}
+
+Shape subtractOP(Shape a, Shape b) {
+    Shape r = a;
+    if (a.sd > -b.sd) {
+        r.sd = a.sd;
+    } else {
+        r.sd = -b.sd;
+    }
+    return r;
+}
+
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+float circleSDF(vec2 p, vec2 c, float r) {
+    return length(p - c) - r;
+}
+
+float octagonSDF(vec2 p, vec2 c, float r) {
+    const vec3 k = vec3(-0.9238795325, 0.3826834323, 0.4142135623);
+    p = abs(p - c);
+    p -= 2.0 * min(dot(vec2(k.x,k.y), p), 0.0) * vec2(k.x,k.y);
+    p -= 2.0 * min(dot(vec2(-k.x,k.y), p), 0.0) * vec2(-k.x,k.y);
+    p -= vec2(clamp(p.x, -k.z * r, k.z * r), r);
+    return length(p) * sign(p.y);
+}
+
+Shape scene(vec2 p) {
+    Shape r1 = Shape(circleSDF(p, vec2(0.2, 0.3), 0.1), 1.0);
+    Shape r2 = Shape(octagonSDF(p, vec2(0.6, 0.5), 0.2), 1.0);
+    return unionOP(r1, r2);
+}
+
+float trace(vec2 p, float dx, float dy) {
+    float t = 0.0;
+    for (int i = 0; i < MAX_STEP && t < MAX_DISTANCE; i++) {
+        Shape r = scene(vec2(p.x + dx * t, p.y + dy * t));
+        if (r.sd < EPSILON) {
+            return r.emissive;
+        }
+        t += r.sd;
+    }
+    return 0.0;
+}
+
+float sampleXY(vec2 p) {
+    float sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        float a = TWO_PI * (float(i) + random(p)) / float(N);
+        sum += trace(p, cos(a), sin(a));
+    }
+    return sum / float(N);
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy / resolution;
-    float col = texture(map, st).r;
-    fragColor = vec4(vec3(col), 1);
+    float col = sampleXY(st);
+    fragColor = vec4(vec3(col), 1.0);
 }
--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 import './styles.css';
-import { Renderer, Program, Texture } from 'gl-layer';
+import { Renderer, Program } from 'gl-layer';
 import frag from './frag.glsl';
 
 const W = 1000;
 const H = 1000;
-const N = 32;
-
-const MAX_STEP = 10;
-const MAX_DISTANCE = 2;
 
 const renderer = new Renderer();
-const gl = renderer.gl;
 
 init();
 addEventHandlers();
@@ -28,101 +23,7 @@ function handleResize() {
     renderer.setSize(W, H);
 }
 
-function union(a, b) {
-    return a.sd < b.sd ? a : b;
-}
-
-function intersect(a, b) {
-    const r = a.sd > b.sd ? b : a;
-    r.sd = a.sd > b.sd ? a.sd: b.sd;
-    return r;
-}
-
-function subtract(a, b) {
-    const r = a;
-    r.sd = (a.sd > -b.sd) ? a.sd : -b.sd;
-    return r;
-}
-
-function length(x, y) {
-    return Math.sqrt(x * x + y * y);
-}
-
-function clamp(x, min, max) {
-    return Math.min(Math.max(x, min), max);
-}
-
-function dot(a, b) {
-    return a.map((_, i) => a[i] * b[i]).reduce((m, n) => m + n);
-}
-
-function circleSDF(x, y, cx, cy, r) {
-    return length(x - cx, y - cy) - r;
-}
-
-function octagonSDF(x, y, cx, cy, r) {
-    const k = [-0.9238795325, 0.3826834323, 0.4142135623];
-    const p = [Math.abs(x - cx), Math.abs(y - cy)];
-
-    let kdotp = Math.min(dot([k[0], k[1]], p), 0);
-    p[0] -= 2.0 * kdotp * k[0];
-    p[1] -= 2.0 * kdotp * k[1];
-
-    kdotp = Math.min(dot([-k[0], k[1]], p), 0);
-    p[0] -= 2.0 * kdotp * -k[0];
-    p[1] -= 2.0 * kdotp * k[1];
-
-    p[0] -= clamp(p[0], -k[2] * r, k[2] * r);
-    p[1] -= r;
-
-    return length(...p) * Math.sign(p[1]);
-}
-
-function scene(x, y) {
-    const r1 = { sd: circleSDF(x, y, 0.2, 0.3, 0.1), emissive: 1 };
-    const r2 = { sd: octagonSDF(x, y, 0.6, 0.5, 0.2), emissive: 1 };
-
-    return union(r1, r2);
-}
-
 function render() {
     const program = new Program(frag);
-
-    let C = new Uint8Array(W * H);
-    for (let y = 0; y < H; y++) {
-        for (let x = 0; x < W; x++) {
-            const s = sample(x / renderer.width, y / renderer.height) * 255;
-            C[y * W + x] = Math.min(s, 255);
-        }
-    }
-
-    const map = new Texture(C, gl.CLAMP_TO_EDGE, gl.CLAMP_TO_EDGE);
-    map.internalFormat = gl.R8;
-    map.format = gl.RED;
-    map.width = W;
-    map.height = H;
-    program.setUniform('map', map);
-
     renderer.render(program);
-}
-
-function sample(x, y) {
-    let sum = 0;
-    for (let i = 0; i < N; i++) {
-        const a = 2 * Math.PI * (i + Math.random()) / N;
-        sum += trace(x, y, Math.cos(a), Math.sin(a));
-    }
-    return sum / N;
-}
-
-function trace(x, y, dx, dy) {
-    let t = 0;
-    for (let i = 0; i < MAX_STEP && t < MAX_DISTANCE; i++) {
-        const r = scene(x + dx * t, y + dy * t);
-        if (r.sd < Number.EPSILON) {
-            return r.emissive;
-        }
-        t += r.sd;
-    }
-    return 0;
 }
